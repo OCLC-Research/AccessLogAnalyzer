27002670d21e3fbe027e96b158d85b7b3ca9c467
Ralph LeVan <levan@oclc.org>
2018-01-18 10:45:07 -0500

    Added support for blacklists

--- src/main/java/org/oclc/accessloganalyzer/CountOfThingsByIP.java
+++ src/main/java/org/oclc/accessloganalyzer/CountOfThingsByIP.java
@@ -10,15 +10,14 @@ import com.martiansoftware.jsap.JSAPException;
 import com.martiansoftware.jsap.JSAPResult;
 import java.util.AbstractMap;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import static org.oclc.accessloganalyzer.CountOfThings.cleanThing;
 
 /**
  *
@@ -26,51 +25,139 @@ import java.util.regex.Pattern;
  */
 public abstract class CountOfThingsByIP extends Analyzer {
 
-    HashMap<String, HashMap<String, Long>> longAddrs=new HashMap<>();
-    HashMap<String, HashMap<String, Long>> shortAddrs=new HashMap<>();
-    TreeSet<String> listOfThings= new TreeSet<>();
+    Counter<String> longCounts=new Counter<>();
+    Counter<String> notBlacklistedLongCounts=new Counter<>();
+    Counter<String> shortCounts=new Counter<>();
+    Counter<String> notBlacklistedShortCounts=new Counter<>();
+    HashMap<String, Counter<String>> longAddrs=new HashMap<>();
+    HashMap<String, Counter<String>> shortAddrs=new HashMap<>();
+    HashMap<String, Counter<String>> notBlacklistedLongAddrs=new HashMap<>();
+    HashMap<String, Counter<String>> notBlacklistedShortAddrs=new HashMap<>();
+    Counter<String> listOfThings= new Counter<>();
     boolean debug;
+    int maxThings=20;
+    long otherCount;
+    private boolean debugGetIPData;
 
-    public abstract String getThing(String line);
+    public abstract List<String> getThings(String line);
     
     @Override
     public void analyze(String line) {
-        String ipAddr = line.substring(0, line.indexOf(' '));
-        if(ipAddr.contains(",")) // host and proxy.  Just use host
-            ipAddr=ipAddr.substring(0, ipAddr.indexOf(','));
-        if(!ipAddr.contains("."))
-            return;  // no IP address
-        HashMap<String, Long> h = longAddrs.get(ipAddr);
-        if(h==null)
-            h=new HashMap<>();
-        String thing=getThing(line);
-        if(thing==null)
-            return;
-        listOfThings.add(thing);
+        for(String thing:getThings(line)) {
+            thing=cleanThing(thing); // let's make sure this is legal junk
+            listOfThings.increment(thing);
+            String ipAddr = line.substring(0, line.indexOf(' '));
+            if(ipAddr.contains(",")) // host and proxy.  Just use host
+                ipAddr=ipAddr.substring(0, ipAddr.indexOf(','));
+            if(!ipAddr.contains("."))
+                return;  // no IP address
+            ipAddr=getIdenticalIP(ipAddr);
+            Counter<String> counter = longAddrs.get(ipAddr);
+            if(counter==null)
+                counter=new Counter<>();
+            counter.increment(thing);
+            counter.increment("__total");
+            longAddrs.put(ipAddr, counter);
+            longCounts.increment(ipAddr);
+            if(!isBlacklisted()) {
+                counter = notBlacklistedLongAddrs.get(ipAddr);
+                if(counter==null)
+                    counter=new Counter<>();
+                counter.increment(thing);
+                counter.increment("__total");
+                notBlacklistedLongAddrs.put(ipAddr, counter);
+                notBlacklistedLongCounts.increment(ipAddr);
+            }
+            // drop last digit
+            String shortAddr = getShortAddress(ipAddr);
+            counter=shortAddrs.get(shortAddr);
+            if(counter==null)
+                counter=new Counter<>();
+            counter.increment(thing);
+            counter.increment("__total");
+            shortAddrs.put(shortAddr, counter);
+            shortCounts.increment(shortAddr);
+            if(!isBlacklisted()) {
+                counter = notBlacklistedShortAddrs.get(shortAddr);
+                if(counter==null)
+                    counter=new Counter<>();
+                counter.increment(thing);
+                counter.increment("__total");
+                notBlacklistedShortAddrs.put(shortAddr, counter);
+                notBlacklistedShortCounts.increment(shortAddr);
+            }
+        }
+    }
+
+    private ArrayList<AbstractMap.SimpleEntry<String, long[]>> getIPData(
+            List<Map.Entry<String, Long>> mostCommonThingsList, 
+            List<Map.Entry<String, Long>> sortedAddrs, 
+            HashMap<String, Counter<String>> addrs) {
+        TreeMap<String, Long> mostCommonThingsMap;
+        boolean tooManyThings=false;
+        Counter<String> thingsForThisAddr;
+        long vals[];
         Long val;
-        val=h.get(thing);
-        if(val==null)
-            val=0L;
-        h.put(thing, val+1);
-        val=h.get("__total");
-        if(val==null)
-            val=0L;
-        h.put("__total", val+1);
-        longAddrs.put(ipAddr, h);
-        // drop last digit
-        String shortAddr = getShortAddress(ipAddr);
-        h=shortAddrs.get(shortAddr);
-        if(h==null)
-            h=new HashMap<>();
-        val=h.get(thing);
-        if(val==null)
-            val=0L;
-        h.put(thing, val+1);
-        val=h.get("__total");
-        if(val==null)
-            val=0L;
-        h.put("__total", val+1);
-        shortAddrs.put(shortAddr, h);
+        String name;
+
+        if(debugGetIPData) {
+            System.out.println("in "+this.getClass().getSimpleName());
+            System.out.println("mostCommonThingsList="+mostCommonThingsList);
+            System.out.println("sortedAddrs="+sortedAddrs);
+        }
+        otherCount=0;
+        if(mostCommonThingsList.size()<listOfThings.size()) {
+            tooManyThings=true;
+            mostCommonThingsMap=new TreeMap<>();
+            for(Map.Entry<String, Long> entry:mostCommonThingsList)
+                mostCommonThingsMap.put(entry.getKey(), entry.getValue());
+            List<Map.Entry<String, Long>> l = listOfThings.most_common();
+            for(int i=maxThings; i<l.size(); i++)
+                otherCount=+l.get(i).getValue();
+        }
+        else
+            mostCommonThingsMap=listOfThings;
+
+        ArrayList<AbstractMap.SimpleEntry<String,long[]>> list=new ArrayList<>();
+        String addr;
+        for(Map.Entry<String, Long> entry:sortedAddrs) {
+            addr=entry.getKey();
+            name=getByAddress(addr+".0");
+            if(name==null) {
+                System.out.println("**** null returned for longAddr "+addr+".0");
+                continue;
+            }
+            thingsForThisAddr=addrs.get(addr);
+            if(thingsForThisAddr==null) {
+                System.out.println("in "+this.getClass().getSimpleName()+": no things for addr="+addr);
+                continue;
+            }
+            if(debugGetIPData) {
+                System.out.println("in getIPData: addr="+addr+", count="+entry.getValue()+", name="+name+", thingsForThisAddr="+thingsForThisAddr);
+            }
+            int i=0;
+            long other=0;
+            if(tooManyThings) {
+                vals=new long[mostCommonThingsList.size()+1];
+                for(String thing: thingsForThisAddr.keySet()) {
+                    if(mostCommonThingsMap.get(thing)==null)
+                        other+=thingsForThisAddr.get(thing);
+                }
+            }
+            else
+                vals=new long[mostCommonThingsList.size()];
+            // create array of counts in mostCommonThings order
+            for(Map.Entry<String, Long> thing:mostCommonThingsList) {
+                val=thingsForThisAddr.get(thing.getKey());
+                if(val==null)
+                    val=0L;
+                vals[i++]=val;
+            }
+            if(tooManyThings)
+                vals[i]=other;
+            list.add(new AbstractMap.SimpleEntry<>(name, vals));
+        }
+        return list;
     }
 
     @Override
@@ -83,12 +170,21 @@ public abstract class CountOfThingsByIP extends Analyzer {
         }
         JSAPResult config = jsap.parse(args);
         debug=config.getBoolean("debug", false);
+        if(debug)
+            System.out.println(this.getClass().getSimpleName()+": debug=true");
     }
 
     @Override
     public void load(String content, Date date) {
         longAddrs.clear();
+        notBlacklistedLongAddrs.clear();
         shortAddrs.clear();
+        notBlacklistedShortAddrs.clear();
+        listOfThings.clear();
+        shortCounts.clear();
+        longCounts.clear();
+        notBlacklistedShortCounts.clear();
+        notBlacklistedLongCounts.clear();
         merge(content, 0);
     }
 
@@ -97,126 +193,114 @@ public abstract class CountOfThingsByIP extends Analyzer {
         Pattern p = Pattern.compile("<"+this.getClass().getSimpleName()+">(.*?)</"+this.getClass().getSimpleName()+">", Pattern.DOTALL);
         Matcher m=p.matcher(content), thingsMatcher;
         if(!m.find()) { // probably a new report
+            if(debug)
+                System.out.println("no content for "+this.getClass().getSimpleName());
             return;
         }
         String usageData = m.group(1);
-        p=Pattern.compile("<ip addr='([^']*)' ([^/]*)/>");
+        p=Pattern.compile("<ip addr='([^']*)' ([^>]*)/>");
         m=p.matcher(usageData);
-        Pattern thingsPattern=Pattern.compile("(\\w*)='([^']*)'");
-        HashMap<String, Long> longMap, shortMap;
-        Long val;
-        long newVal;
+        Pattern thingsPattern=Pattern.compile("\\s*([^=]*)='([^/]*)/([^']*)'");
+        Counter<String> longMap, notBlacklistedLongMap, notBlacklistedShortMap, shortMap;
+        long nbVal, newVal;
         String thing, things;
         while(m.find()) {
             String ipAddr=m.group(1);
             longMap=longAddrs.get(ipAddr);
             if(longMap==null)
-                longMap=new HashMap<>();
+                longMap=new Counter<>();
+            notBlacklistedLongMap=notBlacklistedLongAddrs.get(ipAddr);
+            if(notBlacklistedLongMap==null)
+                notBlacklistedLongMap=new Counter<>();
             // drop last digit
             String shortAddr = getShortAddress(ipAddr);
             shortMap=shortAddrs.get(shortAddr);
             if(shortMap==null)
-                shortMap=new HashMap<>();
+                shortMap=new Counter<>();
+            notBlacklistedShortMap=notBlacklistedShortAddrs.get(shortAddr);
+            if(notBlacklistedShortMap==null)
+                notBlacklistedShortMap=new Counter<>();
 
             // loop through cached values and add them to the hashmaps
             things=m.group(2);
             thingsMatcher=thingsPattern.matcher(things);
             while(thingsMatcher.find()) {
                 thing=thingsMatcher.group(1);
-                listOfThings.add(thing);
                 newVal=Long.parseLong(thingsMatcher.group(2));
-                val=longMap.get(thing);
-                if(val==null)
-                    val=0L;
-                longMap.put(thing, val+newVal);
-                val=shortMap.get(thing);
-                if(val==null)
-                    val=0L;
-                shortMap.put(thing, val+newVal);
+                if(thing.equals("__total")) {
+                    shortCounts.increment(shortAddr, newVal);
+                    longCounts.increment(ipAddr, newVal);
+                }
+                else
+                    listOfThings.increment(thing, newVal);
+                longMap.increment(thing, newVal);
+                shortMap.increment(thing, newVal);
+                
+                nbVal=Long.parseLong(thingsMatcher.group(3));
+                if(nbVal>0) {
+                    notBlacklistedLongMap.increment(thing, nbVal);
+                    notBlacklistedShortMap.increment(thing, nbVal);
+                    if(thing.equals("__total")) {
+                        notBlacklistedShortCounts.increment(shortAddr, nbVal);
+                        notBlacklistedLongCounts.increment(ipAddr, nbVal);
+                    }
+                }
             }
             
             longAddrs.put(ipAddr, longMap);
+            if(!notBlacklistedLongMap.isEmpty())
+                notBlacklistedLongAddrs.put(ipAddr, notBlacklistedLongMap);
             shortAddrs.put(shortAddr, shortMap);
+            if(!notBlacklistedShortMap.isEmpty())
+                notBlacklistedShortAddrs.put(shortAddr, notBlacklistedShortMap);
         }
+        if(debug)
+            System.out.println(this.getClass().getSimpleName()+": #longAddrs="+longAddrs.size()+", #notBlacklistedLongAddrs="+notBlacklistedLongAddrs.size());
     }
 
     @Override
     public Object report() {
         HashMap<String, Object> map=new HashMap<>();
-        ArrayList<AbstractMap.SimpleEntry<String,long[]>> list=new ArrayList<>();
-        ValueComparator bvc=new ValueComparator(shortAddrs);
-        TreeMap<String, HashMap<String, Long>> sortedAddrs=new TreeMap<>(Collections.reverseOrder(bvc));
-        sortedAddrs.putAll(shortAddrs);
-        HashMap<String, Long> h;
-        long vals[];
-        Long val;
-        String longAddr, name;
-        int i;
-        for(String addr:sortedAddrs.keySet()) {
-            // convert shortAddr to longAddr
-            longAddr=null;
-            for(String s:longAddrs.keySet())
-                if(s.startsWith(addr)) {
-                    longAddr=s;
-                    break;
-                }
-//            System.out.println("short="+addr+", long="+longAddr);
-            if(longAddr==null)
-                longAddr=addr;
-            name=getByAddress(longAddr);
-            h=sortedAddrs.get(addr);
-            i=0;
-            vals=new long[listOfThings.size()];
-            for(String thing:listOfThings) {
-                val=h.get(thing);
-                if(val==null)
-                    val=0L;
-                vals[i++]=val;
-            }
-            list.add(new AbstractMap.SimpleEntry<>(name, vals));
+        List<Map.Entry<String, Long>> mostCommonThingsList = listOfThings.most_common(maxThings);
+        if(mostCommonThingsList.size()<listOfThings.size()) {
+            ArrayList<Map.Entry<String, Long>> temp = new ArrayList<>();
+            temp.addAll(mostCommonThingsList);
+            temp.add(new AbstractMap.SimpleEntry<>("other", otherCount));
+            map.put("listOfThings", temp);
+        } else {
+            map.put("listOfThings", mostCommonThingsList);
         }
-        
-        map.put("listOfThings", listOfThings);
-        map.put("ipData", list);
+        map.put("ipData", getIPData(mostCommonThingsList, shortCounts.most_common(), shortAddrs));
+        debugGetIPData=false;
+        map.put("notBlacklistedIpData", getIPData(mostCommonThingsList, notBlacklistedShortCounts.most_common(), notBlacklistedShortAddrs));
+        debugGetIPData=false;
         return map;
     }
 
     @Override
     public String unload() {
-        ValueComparator bvc=new ValueComparator(longAddrs);
-        TreeMap<String, HashMap<String, Long>> sortedAddrs=new TreeMap<>(Collections.reverseOrder(bvc));
-        sortedAddrs.putAll(longAddrs);
         long val;
+        Long zero=0L;
+        String addr;
         StringBuilder sb=new StringBuilder("<"+this.getClass().getSimpleName()+">\n");
-        HashMap<String, Long> h;
-        for(String addr:sortedAddrs.keySet()) {
-            h=sortedAddrs.get(addr);
+        Counter<String> h, h2;
+        List<Map.Entry<String, Long>> mostCommon = longCounts.most_common();
+        for(Map.Entry<String, Long> entry:mostCommon) {
+            addr=entry.getKey();
+            h=longAddrs.get(addr);
+            h2=notBlacklistedLongAddrs.get(addr);
+            if(h2==null)
+                h2=new Counter();
             sb.append("<ip addr='").append(addr).append("'");
             for(String thing:h.keySet()) {
                 val=h.get(thing);
-                if(val>0)
-                    sb.append(" ").append(thing).append("='").append(val).append("'");
+                if(val>0) {
+                    sb.append(" ").append(escapeEntityName(thing)).append("='").append(val).append('/').append(h2.getOrDefault(thing, zero)).append("'");
+                }
             }
-            sb.append("'/>\n");
+            sb.append("/>\n");
         }
         sb.append("</").append(this.getClass().getSimpleName()).append(">\n");
         return sb.toString();
     }
-
-    private class ValueComparator implements Comparator<String> {
-
-        private final Map<String, HashMap<String, Long>> map;
-
-        public ValueComparator(Map<String, HashMap<String, Long>> map) {
-            this.map=map;
-        }
-        
-        @Override
-        public int compare(String a, String b) {
-            if(map.get(a).get("__total").equals(map.get(b).get("__total")))
-                return a.compareTo(b);
-            return (int)(map.get(a).get("__total")-map.get(b).get("__total"));
-        }
-    }
-    
 }
