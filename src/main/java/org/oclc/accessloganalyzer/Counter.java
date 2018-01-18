569e70d4ca5835d63a91f12c9e2bee16ee6e43cc
Ralph LeVan <levan@oclc.org>
2018-01-18 10:57:01 -0500

    A local copy of the Python Counter class

---
+++ src/main/java/org/oclc/accessloganalyzer/Counter.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.oclc.accessloganalyzer;
+
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author levan
+ * @param <T>
+ */
+public class Counter<T extends Comparable<T>> extends TreeMap<T, Long> {
+
+    private static final long serialVersionUID = 1L;
+
+    public Long increment(T key) {
+        return increment(key, 1L);
+    }
+
+    public Long increment(T key, long incrementAmount) {
+        long value=getOrDefault(key, 0L);
+        return put(key, value+incrementAmount);
+    }
+    
+    public List<Map.Entry<T, Long>> most_common() {
+        ValueComparator bvc=new ValueComparator(this);
+        TreeMap<T, Long> sortedThings=new TreeMap<>(bvc);
+        sortedThings.putAll(this);
+        LinkedList<Map.Entry<T, Long>> bob=new LinkedList<>(sortedThings.entrySet());
+        return bob;
+    }
+    
+//    public List<Map.Entry<T, Long>> most_common() {
+//        List<Map.Entry<T, Long>> list =
+//            new LinkedList<>( this.entrySet() );
+//        Collections.sort(list, (Map.Entry<T, Long> o1, Map.Entry<T, Long> o2) -> {
+//            int retval = (o2.getValue()).compareTo( o1.getValue() ); // reverse the order of the values
+//            if(retval==0)
+//                retval=o1.getKey().compareTo(o2.getKey());
+//            return retval;
+//        });
+//
+//        return list;
+//    }
+    
+    public List<Map.Entry<T, Long>> most_common(int numEntriesReturned) {
+        List<Map.Entry<T, Long>> list = most_common();
+        if(list.size()<=numEntriesReturned)
+            return list;
+        return list.subList(0, numEntriesReturned);
+    }
+
+    public void update(Counter<T> counter) {
+        counter.keySet().forEach((key) -> {
+            increment(key, counter.get(key));
+        });
+    }
+    
+    class ValueComparator implements Comparator<T> {
+
+        private final TreeMap<T, Long> map;
+
+        public ValueComparator(TreeMap<T, Long> map) {
+            this.map=map;
+        }
+        
+        @Override
+        public int compare(T a, T b) {
+            long retval=map.get(b)-map.get(a); // reverse the order of number comparisons
+            if(retval==0)
+                retval=a.compareTo(b);
+            if(retval<0)
+                return -1;
+            if(retval>0)
+                return 1;
+            return 0;
+        }
+    }
+}
