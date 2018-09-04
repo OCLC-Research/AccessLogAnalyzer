7d9cebcfe667a71baf5ac13e84388752be750b6a
Ralph LeVan <levan@oclc.org>
2018-09-04 16:53:17 -0400

    Tidied

--- src/main/java/org/oclc/accessloganalyzer/SortedBufferedReader.java
+++ src/main/java/org/oclc/accessloganalyzer/SortedBufferedReader.java
@@ -9,7 +9,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.Comparator;
-import java.util.LinkedList;
 import java.util.PriorityQueue;
 
 /**
